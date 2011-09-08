88d9e9e91ab7d791655999306e3a4bbaa1d136fd
Mike Plavsky <Mike.Plavsky@quest.com>
2011-09-08 17:25:20 +0000

    making diff for two repositories

--- mkdiff.sh
+++ mkdiff.sh
@@ -3,4 +3,25 @@
 repo1=$1
 repo2=$2 
 
-diff -qr $repo1 $repo2 | sort > res.diff
+gdir(){
+ echo $1 | gawk '{match($0,"git@github.com:(.*)/",arr);print arr[1]}' 
+}
+
+dir1=`gdir $repo1`
+dir2=`gdir $repo2`
+
+clone(){
+
+  echo "clonning $1"
+  rm -rf $2
+  git clone $1 $2
+
+}
+
+clone $repo1 $dir1
+clone $repo2 $dir2
+
+echo "creating diff file for $dir1 and $dir2"
+diff -qr $dir1 $dir2 | sort > res.diff    
+
+echo "Done"
