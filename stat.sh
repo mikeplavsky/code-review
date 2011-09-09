d2dc0e87775163b88113fe6c89b1b6e9f8089fe8
Mike Plavsky <Mike.Plavsky@quest.com>
2011-09-09 18:18:45 +0000

    Total changed, all changed are equal

--- stat.sh
+++ stat.sh
@@ -5,13 +5,15 @@ ext_stat() {
   gawk -v re="$2" '{
     
     match( $0, re, arr ); 
-    if (arr[1]) { ext[arr[1]] +=1; }
+    ext[arr[1]] +=1;
 
   }
   
    END { 
 
-     for ( e in ext ) { print " ", e, ext[e] } 
+     for ( e in ext ) { print " ", e, ext[e]; sum += ext[e]; } 
+     print ""
+     print "Total: ", sum
 
   }' $1
 
@@ -24,9 +26,4 @@ echo
 ext_stat $1 "Files .*[.](.*) and"
 
 echo
-echo "New files:"
-echo
 
-ext_stat $1 "Only in .*[.](.*)"
-
-echo
