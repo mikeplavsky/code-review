b349054fef4527a4647a25f4e6aef83223b845ff
Mike Plavsky <Mike.Plavsky@quest.com>
2011-10-26 16:13:11 +0000

    god rif of root directory name

--- creatediff.sh
+++ creatediff.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-dir1=$1
-dir2=$2
+dir1=`echo $1 | sed -e "s/\/$//"` 
+dir2=`echo $2 | sed -e "s/\/$//"`
 
 echo "creating diff files for $dir1 and $dir2"
 
 diff -qr $dir1 $dir2 | sort > all.diff
 
 cat all.diff | grep -v "Only in" > changed.diff
-cat all.diff | grep -v "Only in" | sed -e "s/ and .*$//;s/Files //" > rchanged.diff
+cat changed.diff | sed -e "s/ and .*$//;s/Files $dir1\///" > rchanged.diff
 
 echo "changed.diff done"
 
 path=`dirname $0`
 
 cat all.diff | grep "Only in" | sort | grep -v "$dir2" | $path/sedproc | xargs -l1 find > new.diff
+cat new.diff | sed -e "s/$dir1\///" > rnew.diff
 echo "new.diff done"
 
